189c88166897e338a7d6c1553f1539641e939589
HaireJason-FS <cjhaire@student.fullsail.edu>
2025-11-03 13:05:34 -0500

    added img folder

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
 // Imports your SCSS stylesheet
 import '@/styles/index.scss';
+
+
+// The objective of the game is to find all sets of matching cards within a maximum of 3 tries/attempts. There will be a total of six (6) cards flipped face down at the start. The set will include 3 pairs of matching cards. The player must try to match all three pairs of cards in fewer than 3 attempts. An attempt doesn't represent just one card flip; rather, it involves comparing 2 cards in one try. If the cards don't match, they are flipped back to their face-down state, and the player's remaining attempts are reduced by one.
+
+//At the start of each new game, the cards must reshuffle to different positions and values. Note that we do need to include special cards like Jacks, Queens, Kings, or Aces. (Labeled as the name entails)
+//This project will use HTML, SCSS, and TypeScript.
